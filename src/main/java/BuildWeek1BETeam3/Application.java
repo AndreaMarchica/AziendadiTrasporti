f4670f4b2d61085742e4d3963257e3aac726d47a
OleksandrLarionov <larionov.alex23@gmail.com>
2023-12-19 18:56:55 +0100

    randomize all istances

--- src/main/java/BuildWeek1BETeam3/Application.java
+++ src/main/java/BuildWeek1BETeam3/Application.java
@@ -2,11 +2,13 @@ package BuildWeek1BETeam3;
 
 import BuildWeek1BETeam3.entities.*;
 import BuildWeek1BETeam3.entities.DAO.*;
+import com.github.javafaker.Faker;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import java.time.LocalDate;
+import java.util.Locale;
 import java.util.Random;
 import java.util.function.Supplier;
 
@@ -15,6 +17,8 @@ public class Application {
 
     public static void main(String[] args) {
         EntityManager em = emf.createEntityManager();
+        Faker faker = new Faker(Locale.ITALIAN);
+
 
         Supplier<LocalDate> dateSupplier = () -> {
             Random rdm = new Random();
@@ -23,6 +27,7 @@ public class Application {
             int randomMonth = rdm.nextInt(1,12);
             return LocalDate.of(randomYear, randomMonth,randomDay);
         };
+
 
         TrattaDAO td = new TrattaDAO(em);
         UtenteDAO ud = new UtenteDAO(em);
@@ -43,6 +48,7 @@ public class Application {
         Tratta Linea9 = new Tratta("Piazzale Gioberti", "largo Irneri", 30);
         Tratta Linea10 = new Tratta("Piazza Tommaseo", "Via Valmaura", 40);
 
+
 //        ****************************************SALVATAGGIO NEL DB DELLE TRATTE****************************
 
         td.save(Linea1);
@@ -57,41 +63,21 @@ public class Application {
 
 //        ****************************************CREAZIONE DEGLI UTENTI************************************
 
-        Utente aldo = new Utente("Aldo", "Baglio");
-        Utente giovanni = new Utente("Giovanni", "Storti");
-        Utente giacomo = new Utente("Giacomo", "Poretti");
-        Utente fabrizio = new Utente("Fabrizio" ,"de Andrò");
-        Utente mina = new Utente("Mina", "Mazzini");
-        Utente lidija = new Utente ( "lidija" , "Percan");
-        Utente lucio = new Utente ( "Lucio" , "Battisti");
-        Utente francesco = new Utente ( "Francesco" , "de Gregori");
-        Utente andrea = new Utente ( "Andrea" , "Boccelli");
-        Utente niccolò  = new Utente ( "Niccolò " , "Paganini");
 
-//        ****************************************SALVATAGGIO NEL DB ****************************************
-
-        ud.save(aldo);
-        ud.save(giovanni);
-        ud.save(giacomo);
-        ud.save(fabrizio);
-        ud.save(mina);
-        ud.save(lidija);
-        ud.save(lucio);
-        ud.save(francesco);
-        ud.save(andrea);
-        ud.save(niccolò);
+        for (int i = 0; i < 10; i++){
+            Utente u = new Utente(faker.name().name(), faker.name().lastName());
+            ud.save(u);
+        }
 
 //        **************************************** CREAZIONE DEI TRAM ****************************************
 
-        Tram tram1 = new Tram (70, LocalDate.of(2011, 7, 30));
-        Tram tram2 = new Tram (60, LocalDate.of(2021, 9, 3));
-        Tram tram3 = new Tram (71, LocalDate.of(2010, 2, 15));
+        for (int i = 0; i < 10; i++){
+            Random rndm = new Random();
+            int posti =  rndm.nextInt(50,100);
+            Tram t = new Tram(posti, dateSupplier.get());
+            mtd.save(t);
+        }
 
-//           ****************************************SALVATAGGIO NEL DB ****************************************
-
-        mtd.save(tram1);
-        mtd.save(tram2);
-        mtd.save(tram3);
 
 //        **************************************** CREAZIONE DEI BUS E SALVATAGGIO *******************************
 
@@ -102,43 +88,22 @@ public class Application {
 
  //         **************************************** CREAZIONE DELLE TESSERE *******************************
 
-        Tessera tessera1 = new Tessera(aldo);
-        Tessera tessera2 = new Tessera(giovanni);
-        Tessera tessera3 = new Tessera(giacomo);
-        Tessera tessera4 = new Tessera(mina);
 
- //        ****************************************SALVATAGGIO NEL DB ****************************************
+        ud.getAll().forEach(utente -> {
+            Tessera tessera = new Tessera(utente);
+            tsd.save(tessera);
+        });
 
-        tsd.save(tessera1);
-        tsd.save(tessera2);
-        tsd.save(tessera3);
-        tsd.save(tessera4);
+
 
  //       **************************************** CREAZIONE DEI TITOLI DI VIAGGIO *******************************
 
-        Biglietto biglietto1 = new Biglietto();
-        Biglietto biglietto2 = new Biglietto();
-        Biglietto biglietto3 = new Biglietto();
-        Biglietto biglietto4 = new Biglietto();
-        Biglietto biglietto5 = new Biglietto();
-        Abbonamento abbonamento1 = new Abbonamento(tessera1, VALIDITA.MENSILE);
-        Abbonamento abbonamento2 = new Abbonamento(tessera2, VALIDITA.SETTIMANALE);
-        Abbonamento abbonamento3 = new Abbonamento(tessera3, VALIDITA.MENSILE);
-        Abbonamento abbonamento4 = new Abbonamento(tessera4, VALIDITA.MENSILE);
-        Abbonamento abbonamento5 = new Abbonamento(tessera1, VALIDITA.SETTIMANALE);
 
- //        ****************************************SALVATAGGIO NEL DB ****************************************
 
-/*        tvd.save(abbonamento1);
-        tvd.save(abbonamento2);
-        tvd.save(abbonamento3);
-        tvd.save(abbonamento4);
-        tvd.save(abbonamento5);
-        tvd.save(biglietto1);
-        tvd.save(biglietto2);
-        tvd.save(biglietto3);
-        tvd.save(biglietto4);
-        tvd.save(biglietto5);*/
+        tsd.getAll().forEach( tessera -> {
+            Abbonamento abbonamento = new Abbonamento(tessera, VALIDITA.MENSILE);
+            tvd.save(abbonamento);
+        });
 
 
 
@@ -158,8 +123,12 @@ public class Application {
         ped.save(riv4);
         ped.save(riv5);
         tvd.save(aRiv1.stampaBiglietto());
+
+
         em.close();
         emf.close();
+
+        System.out.printf("**********************************************************************************");
         System.out.println("Hello Moto!");
     }
 }
