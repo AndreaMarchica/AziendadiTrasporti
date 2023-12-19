ac549b5543233f8a230e8505caf312e97b6ef0a4
AndreaMarchica <andrea.marchica94@gmail.com>
2023-12-19 19:36:38 +0100

    add random rivenditori

--- src/main/java/BuildWeek1BETeam3/Application.java
+++ src/main/java/BuildWeek1BETeam3/Application.java
@@ -10,6 +10,7 @@ import javax.persistence.Persistence;
 import java.time.LocalDate;
 import java.util.Locale;
 import java.util.Random;
+import java.util.UUID;
 import java.util.function.Supplier;
 
 public class Application {
@@ -21,10 +22,10 @@ public class Application {
 
         Supplier<LocalDate> dateSupplier = () -> {
             Random rdm = new Random();
-            int randomYear = rdm.nextInt(2000,2024);
-            int randomDay = rdm.nextInt(1,29);
-            int randomMonth = rdm.nextInt(1,12);
-            return LocalDate.of(randomYear, randomMonth,randomDay);
+            int randomYear = rdm.nextInt(2000, 2024);
+            int randomDay = rdm.nextInt(1, 29);
+            int randomMonth = rdm.nextInt(1, 12);
+            return LocalDate.of(randomYear, randomMonth, randomDay);
         };
 
 
@@ -50,7 +51,7 @@ public class Application {
 
 //        ****************************************SALVATAGGIO NEL DB DELLE TRATTE****************************
 
-        td.save(Linea1);
+/*        td.save(Linea1);
         td.save(Linea3);
         td.save(Linea4);
         td.save(Linea5);
@@ -58,39 +59,41 @@ public class Application {
         td.save(Linea7);
         td.save(Linea8);
         td.save(Linea9);
-        td.save(Linea10);
+        td.save(Linea10);*/
 
 //    ****************************************CREAZIONE E SALVATAGGIO DEGLI UTENTI************************************
 
-        for (int i = 0; i < 10; i++){
+/*        for (int i = 0; i < 10; i++) {
             Utente u = new Utente(faker.name().name(), faker.name().lastName());
             ud.save(u);
-        }
+        }*/
 
 
 //   **************************************** CREAZIONE E SALVATAGGIO DEI TRAM ****************************************
 
-        for (int i = 0; i < 10; i++){
+/*
+        for (int i = 0; i < 10; i++) {
             Random rndm = new Random();
-            int posti =  rndm.nextInt(50,100);
+            int posti = rndm.nextInt(50, 100);
             Tram t = new Tram(posti, dateSupplier.get());
             mtd.save(t);
         }
+*/
 
 
 //        **************************************** CREAZIONE DEI BUS E SALVATAGGIO *******************************
 
-        for (int i = 0; i < 10; i++) {
-            Autobus a = new Autobus(50,dateSupplier.get());
+/*        for (int i = 0; i < 10; i++) {
+            Autobus a = new Autobus(50, dateSupplier.get());
             mtd.save(a);
-        }
+        }*/
 
  //      **************************************** CREAZIONE E SALVATAGGIO DELLE TESSERE *******************************
 
-        ud.getAll().forEach(utente -> {
+/*        ud.getAll().forEach(utente -> {
             Tessera tessera = new Tessera(utente);
             tsd.save(tessera);
-        });
+        });*/
 
  //       **************************************** CREAZIONE DEI TITOLI DI VIAGGIO *******************************
 
@@ -100,42 +103,32 @@ public class Application {
         Biglietto biglietto4 = new Biglietto();
         Biglietto biglietto5 = new Biglietto();
 
-        tsd.getAll().forEach( tessera -> {
+/*        tsd.getAll().forEach(tessera -> {
             Abbonamento abbonamento = new Abbonamento(tessera, VALIDITA.MENSILE);
             tvd.save(abbonamento);
-        });
+        });*/
 
 
-        tsd.getAll().forEach( tessera -> {
-            Abbonamento abbonamento = new Abbonamento(tessera, VALIDITA.MENSILE);
-            tvd.save(abbonamento);
-        });
-
-
-
- //         **************************************** CREAZIONE DEI TITOLI DI VIAGGIO *******************************
-        for (int i = 0; i <10; i++){
+ //    **************************************** CREAZIONE DEI PUNTI DI EMISSIONE *******************************
+/*        for (int i = 0; i < 10; i++) {
             Random rdm = new Random();
             int a = rdm.nextInt(0, 2);
             boolean b;
-            b = a == 1 ;
+            b = a == 1;
             RivenditoreAutomatico raut = new RivenditoreAutomatico(faker.address().streetAddress(), b);
             ped.save(raut);
+        }*/
+        for (int i = 0; i < 10; i++) {
+            RivenditoreAutorizzato raut = new RivenditoreAutorizzato(faker.address().streetAddress());
+            ped.save(raut);
         }
-/*        RivenditoreAutomatico aRiv1 = new RivenditoreAutomatico("EUR", true);
-        RivenditoreAutomatico aRiv2 = new RivenditoreAutomatico("Garbatella", false);
-        RivenditoreAutomatico aRiv3 = new RivenditoreAutomatico("Centocelle", true);
+/*
         RivenditoreAutorizzato riv4 = new RivenditoreAutorizzato("Trastevere");
         RivenditoreAutorizzato riv5 = new RivenditoreAutorizzato("Palatino");*/
 
  //           ****************************************SALVATAGGIO NEL DB ****************************************
 
-/*        ped.save(aRiv1);
-        ped.save(aRiv2);
-        ped.save(aRiv3);
-        ped.save(riv4);
-        ped.save(riv5);
-        tvd.save(aRiv1.stampaBiglietto());
+        tvd.save(ped.getById(UUID.fromString("08105258-0440-43fc-9a92-8e7587215e1d")).stampaBiglietto());
 
 
         em.close();
@@ -145,3 +138,4 @@ public class Application {
         System.out.println("Hello Moto!");
     }
 }
+
